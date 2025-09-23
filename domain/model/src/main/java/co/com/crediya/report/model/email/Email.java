872525df84c74a98e04732f741f9fbbf8e3c64a8
Juan Pablo <juanpasan2000@gmail.com>
2025-09-23 18:11:39 -0500

    Auth webclient and Ses configuration

---
+++ domain/model/src/main/java/co/com/crediya/report/model/email/Email.java
@@ -0,0 +1,20 @@
+package co.com.crediya.report.model.email;
+
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Email {
+
+    List<String> emails;
+
+}
---
+++ infrastructure/driven-adapters/ses-notification/src/main/java/co/com/crediya/report/ses/config/SESConfig.java
@@ -0,0 +1,24 @@
+package co.com.crediya.report.ses.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.metrics.MetricPublisher;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesAsyncClient;
+
+@Configuration
+public class SESConfig {
+
+    @Bean
+    public SesAsyncClient configSesDailyReport(SESProperties properties, MetricPublisher publisher) {
+        return SesAsyncClient.builder()
+                .region(Region.of(properties.region()))
+                .overrideConfiguration(o -> o.addMetricPublisher(publisher))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials
+                        .create(properties.accessKeyId(), properties.secretAccessKey())))
+                .build();
+    }
+
+}
